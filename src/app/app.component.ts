2eea746fb4d6c6e8f55ceaba62d5ebe8f7f9c059
Maxis <maxi.schuster@icloud.com>
2023-05-09 03:32:13 +0200

    header, page title & routing styling is just for development and not final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webBandas';
+  // Código para cambiar el título de la página
+  //! mira en el app-routing.module.ts para cambiar los títulos de las páginas
+  constructor(
+    private router: Router,
+    private titleService: Title
+  ) { }
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route: ActivatedRoute = this.router.routerState.root;
+          let routeTitle = '';
+          while (route!.firstChild) {
+            route = route.firstChild;
+          }
+          if (route.snapshot.data['title']) {
+            routeTitle = route!.snapshot.data['title'];
+          }
+          return routeTitle;
+        })
+      )
+      .subscribe((title: string) => {
+        if (title) {
+          this.titleService.setTitle(`${title}`);
+        }
+      });
+  }
+  // icono de registro/login
+  faUser = faUser
 }
