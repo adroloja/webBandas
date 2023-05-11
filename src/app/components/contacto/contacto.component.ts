3bbbd5905f46d56fc2a0e1b151cddadc6312eee7
Maxis <maxi.schuster@icloud.com>
2023-05-12 00:11:00 +0200

    finished without polish and movile

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.css']
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void { }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      const contactDetails = this.contactForm.value;
+      console.log('Contact Details: ', contactDetails);
+      // Your backend logic here
+    }
+  }
 }
